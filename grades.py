ffd469523aa1250f41c93d5d4ed57bdfbb96e376
shroukmah7awi <shroukelmahallwy@gmail.com>
2025-10-03 21:13:52 +0300

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,13 @@
+total_degree=int(input("Enter your degree : "))
+if(total_degree>85):
+ print("Exellent")
+
+elif(total_degree>75):
+  print("Very good")
+
+elif(total_degree>65):
+   print("good")
+
+else:
+   print("fail")
+
